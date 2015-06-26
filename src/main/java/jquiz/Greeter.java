815db0130e5a330f1f3f5cd6d74f749352b91e25
Andrey Artyukhov <andrey.artyukhov@constant.obninsk.ru>
2015-06-26 17:43:47 +0300

    First commit!

---
+++ src/main/java/jquiz/Greeter.java
@@ -0,0 +1,7 @@
+package jquiz;
+
+public class Greeter {
+  public String sayHello() {
+    return "Hello world!";
+  }
+}
---
+++ src/main/java/jquiz/GreetingController.java
@@ -0,0 +1,17 @@
+package jquiz;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public Greeting greeting() {
+        return new Greeting("msg");
+    }
+
+}
